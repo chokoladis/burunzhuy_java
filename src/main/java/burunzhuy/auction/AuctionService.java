4bdc3c93be422d36f9ddc9eaf6dfba2b88ff4f1a
chokoladis <igor2011__igor@mail.ru>
2026-05-24 17:59:58 +0400

    init project

---
+++ src/main/java/burunzhuy/auction/AuctionService.java
@@ -0,0 +1,11 @@
+package burunzhuy.auction;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuctionService {
+    public String getId()
+    {
+        return "test 11";
+    }
+}
---
+++ src/main/java/burunzhuy/auth/AuthController.java
@@ -0,0 +1,4 @@
+package burunzhuy.auth;
+
+public class AuthController {
+}
