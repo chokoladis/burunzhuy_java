b80a191698bc7a13c70e444a093d633af324010b
chokoladis <igor2011__igor@mail.ru>
2026-08-02 18:06:37 +0400

    added cron cleaner for refresh tokens, password restore

--- src/main/java/burunzhuy/cron/auth/PasswordTasks.java
+++ src/main/java/burunzhuy/cron/auth/PasswordTasks.java
@@ -1,11 +1,27 @@
 package burunzhuy.cron.auth;
 
+import burunzhuy.entity.auth.PasswordRestore;
+import burunzhuy.repository.PasswordRestoreRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Component
+@RequiredArgsConstructor
 public class PasswordTasks {
-    @Scheduled(cron = "0 0 2 * * *")
-    public void cleanOld()
-    {
 
+    private final PasswordRestoreRepository passwordRestoreRepository;
+
+    @Scheduled(cron = "0 0 2 * * *")
+    public void deleteOld()
+    {
+        Collection<PasswordRestore> passwordRestores = passwordRestoreRepository.findByCreatedAtBefore(LocalDateTime.now().minusDays(2L));
+        if (passwordRestores.isEmpty())
+            return;
+
+        passwordRestoreRepository.deleteAllInBatch(passwordRestores);
     }
 }
--- src/main/java/burunzhuy/cron/auth/TokenTasks.java
+++ src/main/java/burunzhuy/cron/auth/TokenTasks.java
@@ -1,16 +1,40 @@
 package burunzhuy.cron.auth;
 
+import burunzhuy.entity.security.RefreshToken;
 import burunzhuy.repository.security.RefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+@Component
 @RequiredArgsConstructor
 public class TokenTasks {
     private final RefreshTokenRepository refreshTokenRepository;
 
     @Scheduled(cron = "0 0 2 * * *")
-    public void cleanOld()
+    public void revokeExpired()
     {
+        Collection<RefreshToken> refreshTokens = refreshTokenRepository.findByIsRevokedFalseAndExpiredAtBefore(LocalDateTime.now());
+        if (refreshTokens.isEmpty())
+            return;
 
+        for(RefreshToken token : refreshTokens) {
+            token.setIsRevoked(true);
+        }
+
+        refreshTokenRepository.saveAllAndFlush(refreshTokens);
+    }
+
+    @Scheduled(cron = "0 5 2 * * *")
+    public void deleteOld()
+    {
+        Collection<RefreshToken> refreshTokens = refreshTokenRepository.findByCreatedAtBefore(LocalDateTime.now().minusDays(30L));
+        if (refreshTokens.isEmpty())
+            return;
+
+        refreshTokenRepository.deleteAllInBatch(refreshTokens);
     }
 }
